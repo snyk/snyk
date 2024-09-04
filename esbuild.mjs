e3c03e340a620f5b4a925a84613dc01a035ac9bf
Luke Watts <luke@snyk.io>
2024-09-04 08:43:57 +0200

    spike: partial implementation of license behaviour
    Still to be resolved:
    * Source URL
    * Repo URL

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,12 +1,34 @@
-import * as esbuild from 'esbuild'
+import * as esbuild from 'esbuild';
+import esbuildPluginLicense from 'esbuild-plugin-license';
 
 await esbuild.build({
   entryPoints: ['./src/cli/index.ts'],
   bundle: true,
-  platform: "node",
-  target: "node18",
-  outfile: "dist/cli/index.js",
-  external: [
-    "*.node",
-  ]
-})
+  platform: 'node',
+  target: 'node18',
+  outfile: 'dist/cli/index.js',
+  external: ['*.node'],
+  plugins: [
+    esbuildPluginLicense({
+      thirdParty: {
+        includePrivate: false,
+        output: {
+          file: 'dist/cli/thirdPartyNotice.tmp.json',
+          template(deps) {
+            return JSON.stringify(
+              deps.map((dep) => ({
+                name: dep.packageJson.name,
+                version: dep.packageJson.version,
+                author: dep.packageJson.author?.name || null,
+                repository: dep.packageJson.repository,
+                source: dep.packageJson.source || `https://registry.npmjs.org/${dep.packageJson.name}/-/${dep.packageJson.name}-${dep.packageJson.version}.tgz`,
+                license: dep.packageJson.license,
+                licenseText: dep.licenseText,
+              })),
+            );
+          },
+        },
+      },
+    }),
+  ],
+});
