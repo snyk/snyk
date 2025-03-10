e6f0252c5d2515c75f2f9d21eaab49134c54dea4
CatalinSnyk <catalin.iuga@snyk.io>
2025-03-10 16:48:41 +0200

    chore: add errors and a few tests

--- cliv2/internal/cliv2/cliv2_test.go
+++ cliv2/internal/cliv2/cliv2_test.go
@@ -14,6 +14,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/snyk/error-catalog-golang-public/snyk_errors"
 	"github.com/snyk/go-application-framework/pkg/app"
 	"github.com/snyk/go-application-framework/pkg/configuration"
 	"github.com/snyk/go-application-framework/pkg/local_workflows/output_workflow"
@@ -586,4 +587,67 @@ func Test_GetErrorDisplayStatus(t *testing.T) {
 			assert.Equal(t, hasBeenDisplayed, tc.expected)
 		})
 	}
+}
+
+func Test_GetErrorFromFile(t *testing.T) {
+	// returns an ExitError instances with the specified exit code
+	getExitError := func(exitCode int) error {
+		var err error
+		command := fmt.Sprintf("exit %d", exitCode)
+		if runtime.GOOS == "windows" {
+			cmd := exec.Command("cmd", "/C", command)
+			err = cmd.Run()
+		} else {
+			cmd := exec.Command("sh", "-c", command)
+			err = cmd.Run()
+		}
+
+		return err
+	}
+
+	// Setup
+	jsonAPIErrorBytes := []byte(`{"jsonapi":{"version":"1.0"},"errors":[{"id":"1","links":{"about":"https://docs.snyk.io/scan-with-snyk/error-catalog#snyk-os-7001"},"status":"504","code":"SNYK-OS-7001","title":"Request to Snyk API timeout","detail":"connection is timedout","meta":{"links":["https://status.snyk.io/"],"isErrorCatalogError":true,"classification":"UNEXPECTED"}}]}`)
+	validFilePath := path.Join(t.TempDir(), "valid-err-file")
+	err := os.WriteFile(validFilePath, jsonAPIErrorBytes, 0664)
+	assert.Nil(t, err)
+
+	notJsonBytes := []byte(`this is not a valid json`)
+	invalidFilePath := path.Join(t.TempDir(), "invalid-err-file")
+	err = os.WriteFile(invalidFilePath, notJsonBytes, 0664)
+	assert.Nil(t, err)
+
+	// notFoundFilePath := path.Join(t.TempDir(), "not-found-file")
+
+	jsonApiError, err := snyk_errors.FromJSONAPIErrorBytes(jsonAPIErrorBytes)
+	assert.Nil(t, err)
+	jsonAPIError := jsonApiError[0]
+
+	config := configuration.NewWithOpts(configuration.WithAutomaticEnv())
+
+	t.Run("does not send error for exit code 0", func(t *testing.T) {
+		exitCodeErr := getExitError(0)
+		sentErr, err := cliv2.GetErrorFromFile(exitCodeErr, validFilePath, config)
+
+		assert.Nil(t, sentErr)
+		assert.Nil(t, err)
+	})
+
+	t.Run("does not send error for exit code 1", func(t *testing.T) {
+		exitCodeErr := getExitError(1)
+		sentErr, err := cliv2.GetErrorFromFile(exitCodeErr, validFilePath, config)
+
+		assert.Nil(t, sentErr)
+		assert.Nil(t, err)
+	})
+
+	t.Run("sends error for exit code 2", func(t *testing.T) {
+		exitCodeErr := getExitError(2)
+		sentErr, err := cliv2.GetErrorFromFile(exitCodeErr, validFilePath, config)
+
+		snykErr := snyk_errors.Error{}
+		assert.ErrorIs(t, sentErr, exitCodeErr)
+		assert.ErrorAs(t, sentErr, &snykErr)
+		assert.Equal(t, snykErr.ErrorCode, jsonAPIError.ErrorCode)
+		assert.Nil(t, err)
+	})
 }
